7d513c2fea0dbc8228e7e243402b407de2a4bfba
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2023-03-11 08:44:39 +0100

    add node test

--- generateSpecs.js
+++ generateSpecs.js
@@ -9,14 +9,13 @@ test('adds 1 + 2 to equal 3', () => {
 
 let template = Handlebars.compile(jestSpec);
 
-result = template();
+let result = template();
 
 for (let i = 0; i < 5000; i++) {
   fs.writeFile(`./jest/test/sum.${i}.test.js`, result, err => {
     if (err) {
       console.error(err);
     }
-    // file written successfully
   });
 }
 
@@ -37,6 +36,25 @@ for (let i = 0; i < 5000; i++) {
     if (err) {
       console.error(err);
     }
-    // file written successfully
+  });
+}
+
+const nodeSpec = `
+import sum from '../sum.mjs';
+import assert from 'node:assert/strict';
+import test from 'node:test';
+test('ads 1 + 2 to equal 3', (t) => {
+  assert.strictEqual(sum(1, 2), 3);
+});
+`;
+
+template = Handlebars.compile(nodeSpec);
+result = template();
+
+for (let i = 0; i < 5000; i++) {
+  fs.writeFile(`./node/test/sum.${i}.test.mjs`, result, err => {
+    if (err) {
+      console.error(err);
+    }
   });
 }
---
+++ node/test/sum.3831.test.mjs
@@ -0,0 +1,7 @@
+
+import sum from '../sum.mjs';
+import assert from 'node:assert/strict';
+import test from 'node:test';
+test('ads 1 + 2 to equal 3', (t) => {
+  assert.strictEqual(sum(1, 2), 3);
+});
